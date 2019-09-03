b18cc468b7a4c7c644e7e8f250e8c655e03b96b9
sirRippchen <woebbi@googlemail.com>
2019-09-03 10:28:16 +0200

    starting with the logic added activity for login page

--- app/src/main/java/com/woebbi/a20190806_002_layouts/LoginActivity.java
+++ app/src/main/java/com/woebbi/a20190806_002_layouts/LoginActivity.java
@@ -1,16 +1,23 @@
 package com.woebbi.a20190806_002_layouts;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import androidx.appcompat.app.AppCompatActivity;
+
+/**
+ * Loginactivity ist fürs activity_login zuständig
+ *
+ * @author woebbi
+ * @
+ */
+
+public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         //LAyout für diese activity setzten
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
     }
 }
