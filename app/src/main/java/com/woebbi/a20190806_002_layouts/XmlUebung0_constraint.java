aba8c448627b156f4988bae6a14ec44cb51f2435
sirRippchen <woebbi@googlemail.com>
2019-09-17 10:52:14 +0200

    xml exercise#2

---
+++ app/src/main/java/com/woebbi/a20190806_002_layouts/XmlUebung0_constraint.java
@@ -0,0 +1,16 @@
+package com.woebbi.a20190806_002_layouts;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class XmlUebung0_constraint extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        //LAyout für diese activity setzten
+        setContentView(R.layout.activity_xmluebung0_constraint);
+    }
+}
