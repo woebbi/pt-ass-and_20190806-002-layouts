665da21b44ac517e740df4c2453e5a02ef9ad0d7
sirRippchen <woebbi@googlemail.com>
2019-10-01 12:52:30 +0200

    shared preferences zum speichern des first run und change to mainActivity wenn falls

--- app/src/main/java/com/woebbi/a20190806_002_layouts/LoginActivity.java
+++ app/src/main/java/com/woebbi/a20190806_002_layouts/LoginActivity.java
@@ -33,35 +33,50 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
     private Button lAButtonAbort;
     private Spinner lAEmailProviderList;
 
+    private boolean firstRun = true;
+
+    private SharedPreferences sp;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //LAyout für diese activity setzten
-        setContentView(R.layout.activity_login);
 
-        //assosiate internal class variable with the loginActivity IDs
-        lAUsernameInput       = findViewById(R.id.loginActivityUsernameInput);
-        lAUsernameText        = findViewById(R.id.loginActivityUsernameText);
-        lAPasswordInput       = findViewById(R.id.loginActivityPasswordInput);
-        lAPasswordText        = findViewById(R.id.loginActivityPasswordText);
-        lAButtonAbort         = findViewById(R.id.loginActivityButtonAbort);
-        lAButtonSend          = findViewById(R.id.loginActivityButtonSend);
-        lAEmailProviderList   = findViewById(R.id.loginActivityEmailProviderList);
+        sp = getSharedPreferences("com.woebbi.a20190806_002_layouts", Context.MODE_PRIVATE);
 
-        //2 wege - der anonyme  - nur lokal für den einen button
-        /*
-        lAButtonSend.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                //anweisung
-            }
-        });
-        */
+        //static müssen nicht vorher als objekt angelegt werden und dann direkt mit CLASSNAME.METHODE/KONSTANTE drauf zugreifen
+        firstRun = sp.getBoolean("firstRun", true);
+        if(firstRun){
+            setContentView(R.layout.activity_login);
+            //assosiate internal class variable with the loginActivity IDs
+            lAUsernameInput = findViewById(R.id.loginActivityUsernameInput);
+            lAUsernameText = findViewById(R.id.loginActivityUsernameText);
+            lAPasswordInput = findViewById(R.id.loginActivityPasswordInput);
+            lAPasswordText = findViewById(R.id.loginActivityPasswordText);
+            lAButtonAbort = findViewById(R.id.loginActivityButtonAbort);
+            lAButtonSend = findViewById(R.id.loginActivityButtonSend);
+            lAEmailProviderList = findViewById(R.id.loginActivityEmailProviderList);
 
-        //2 wege - der andere weg - global
-        // oben "implements..."-> onclicklistener -> auf die rote birne -> dann kommt unten @override publich void onclickviewview
-        lAButtonSend.setOnClickListener(this); //nicht vergessen sonst doof
-        lAButtonAbort.setOnClickListener(this);
+            //2 wege - der anonyme  - nur lokal für den einen button
+            /*
+            lAButtonSend.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    //anweisung
+                }
+            });
+            */
+
+            //2 wege - der andere weg - global
+            // oben "implements..."-> onclicklistener -> auf die rote birne -> dann kommt unten @override publich void onclickviewview
+            lAButtonSend.setOnClickListener(this); //nicht vergessen sonst doof
+            lAButtonAbort.setOnClickListener(this);
+        } else {
+            Intent newIntent = new Intent(LoginActivity.this, MainActivity.class);
+            startActivity(newIntent);
+            finish();
+
+        }
     }
 
 
@@ -71,29 +86,32 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         if (view.getId() == lAButtonSend.getId()) {
             //Toast.makeText(this, "Anmelden", Toast.LENGTH_LONG).show();
             //lAUsernameText.setTextColor(getColor(R.color.colorPrimaryDark));
-            if(lAUsernameInput.length() > 0 && lAPasswordInput.length() > 0){
+            if (lAUsernameInput.length() > 0 && lAPasswordInput.length() > 0) {
                 String username = lAUsernameInput.getText().toString();
                 String password = lAPasswordInput.getText().toString();
                 String usernameDB = "b";
                 String passwordDB = "b";
-                if(username.equals(usernameDB) && password.equals(passwordDB)){
+                if (username.equals(usernameDB) && password.equals(passwordDB)) {
                     //Login erfolgreich
                     //alleshierrein
-                    Toast.makeText(this,"erfolgreich", Toast.LENGTH_LONG).show();
+                    //sharedpreference für firstlogin bei geklückten login auf false setzen
+                    firstRun = false;
+                    SharedPreferences.Editor spe = sp.edit();
+                    spe.putBoolean("firstRun",firstRun);
+                    spe.commit();
+
                     Intent newIntent = new Intent(LoginActivity.this, MainActivity.class);
+                    newIntent.putExtra("user", username);
                     startActivity(newIntent);
-
-
-
-
-                }else{
-                    Toast.makeText(this,"Benutzername oder Password Falsch", Toast.LENGTH_LONG).show();
+                    finish();
+                } else {
+                    Toast.makeText(this, "Benutzername oder Password Falsch", Toast.LENGTH_LONG).show();
                 }
-            }else {
+            } else {
                 Toast.makeText(this, "Alle Felder ausfüllen", Toast.LENGTH_LONG).show();
             }
         } else if (view.getId() == lAButtonAbort.getId()) {
-            lAUsernameText.setText(R.string.loginActivityPassword);
+
             Toast.makeText(this, "Abrechen", Toast.LENGTH_LONG).show();
         }
     }
