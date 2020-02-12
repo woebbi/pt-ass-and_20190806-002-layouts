0d225cb232178b9ff52b144063a82cfa43be4335
sirRippchen <woebbi@googlemail.com>
2020-02-12 14:17:36 +0100

    test with recyclerview

--- app/src/main/java/com/woebbi/a20190806_002_layouts/MainActivity.java
+++ app/src/main/java/com/woebbi/a20190806_002_layouts/MainActivity.java
@@ -1,6 +1,5 @@
 package com.woebbi.a20190806_002_layouts;
 
-import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,7 +9,6 @@ import androidx.drawerlayout.widget.DrawerLayout;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.provider.ContactsContract;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -134,7 +132,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 }
                 return;
             default:
-                Toast.makeText(this, resultCode + "Result <???????> Request" + requestCode, Toast.LENGTH_LONG).show();
+                Toast.makeText(this, resultCode + "Result <?default?> Request" + requestCode, Toast.LENGTH_LONG).show();
                 return;
 
         }
